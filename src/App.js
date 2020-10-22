845a3393d9805594980dfd78fae6f79838aa35cf
Franz Bewerunge <franz.bewerunge@tier.app>
2020-10-23 00:12:01 +0200

    Add images to homepage

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import './App.css';
-import HomePage from './homepage.component'
+import HomePage from './pages/homepage/homepage.component'
+import './pages/homepage/homepage.styles.scss'
+
+
 
 function App() {
   return (
     <div className='App'>
-        <HomePage></HomePage>
+        <HomePage className='homepage'></HomePage>
     </div>
   );
 }
